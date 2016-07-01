07f035fe0df6d2747795ef5c11f9120bd5a3b69d
lyy <511455842@qq.com>
2016-07-01 23:40:32 +0800

    boss自聘跳转适配6.0

--- BossTransfer/app/src/main/java/com/example/arial/bosstransfer/MainActivity.java
+++ BossTransfer/app/src/main/java/com/example/arial/bosstransfer/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.arial.bosstransfer;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -10,6 +15,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.arial.bosstransfer.boss.BossAdapter;
 import com.example.arial.bosstransfer.boss.BossEntity;
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             "http://static.gaoshouyou.com/i/f6/cb/2cf65de5e4cb0e0739b162fa4593d8ed.png"
     };
     private ListView mList;
+    private View     mItemView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +45,30 @@ public class MainActivity extends AppCompatActivity {
         mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                TurnHelp.turn(MainActivity.this, findViewById(android.R.id.content), view);
+                mItemView = view;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                        && checkSelfPermission(Settings.ACTION_MANAGE_OVERLAY_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+                    requestAlertWindowPermission();
+                } else {
+                    TurnHelp.turn(MainActivity.this, findViewById(android.R.id.content), view);
+                }
+
             }
         });
     }
 
+    private void requestAlertWindowPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 1);
+        }
+    }
 
 
     private List<BossEntity> getData() {
         List<BossEntity> list = new ArrayList<>();
-        int i = 0;
+        int              i    = 0;
         for (String img : mIcons) {
             BossEntity entity = new BossEntity();
             entity.setImg(img);
@@ -56,5 +77,17 @@ public class MainActivity extends AppCompatActivity {
             i++;
         }
         return list;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.canDrawOverlays(this)) {
+                TurnHelp.turn(MainActivity.this, findViewById(android.R.id.content), mItemView);
+            } else {
+                Toast.makeText(this, "申请悬浮框权限失败", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
